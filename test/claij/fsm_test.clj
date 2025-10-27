861bd66279818bf1c58f0d9e54611fe003957bd3
jules <jules_gosnell@yahoo.com>
2025-10-27 21:34:28 +0000

    check proposer is in-role

--- test/claij/fsm_test.clj
+++ test/claij/fsm_test.clj
@@ -11,17 +11,14 @@
 
    :xitions
    [{:id ["a" "a"]
+     :roles ["lead"]
      :schema {"type" "number"}}
     {:id ["a" "b"]
+     :roles ["lead" "dev"]
      :schema {"type" "string"}}
     {:id ["a" "c"]
+     :roles ["tools"]
      :schema {"type" "boolean"}}]})
-
-(def proposal
-  {
-   "$id" "TODO"
-   "id" ["a" "b"]
-   "document" "a test xition"})
 
 (deftest test-fsm
   (testing "make-xitions-schema"
@@ -32,19 +29,46 @@
        "oneOf"
        [{"properties"
          {"id" {"const" ["a" "a"]}
+          "roles" {"type" "array" "items" {"type" "string"}}
           "document" {"type" "number"}}}
         {"properties"
          {"id" {"const" ["a" "b"]}
+          "roles" {"type" "array" "items" {"type" "string"}}
           "document" {"type" "string"}}}
         {"properties"
          {"id" {"const" ["a" "c"]}
+          "roles" {"type" "array" "items" {"type" "string"}}
           "document" {"type" "boolean"}}}]}
       (make-xitions-schema fsm "a"))))
-  (testing "xition"
-    (is
-     (=
-      "b"
-      (xition
-       fsm
-       "a"
-       proposal)))))
+  (testing "xition: "
+    (testing "matches"
+      (is
+       (=
+        "b"
+        (xition
+         fsm
+         "a"
+         {"$id" "TODO"
+          "id" ["a" "b"]
+          "roles" ["dev" "docs"]
+          "document" "a test xition"}))))
+    (testing "doesn't match"
+      (is
+       (false?
+        (xition
+         fsm
+         "a"
+         {"$id" "TODO"
+          "id" ["a" "b"]
+          "roles" ["dev" "docs"]
+          "document" 0}))))
+    (testing "out of role"
+      (is
+       (nil?
+        (xition
+         fsm
+         "a"
+         {"$id" "TODO"
+          "id" ["a" "b"]
+          "roles" ["docs"]
+          "document" "a test xition"}))))))
