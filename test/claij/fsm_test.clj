67015112abc62a34a0f9e8f627329ebb6365a1e4
jules <jules_gosnell@yahoo.com>
2025-11-03 16:39:35 +0000

    checkpoint

--- test/claij/fsm_test.clj
+++ test/claij/fsm_test.clj
@@ -2,11 +2,12 @@
   (:require
    [clojure.tools.logging :as log]
    [clojure.data.json :refer [write-str]]
+   [clojure.pprint :refer [pprint]]
    [clojure.test :refer [deftest testing is]]
    [m3.uri :refer [parse-uri]]
    [m3.validate :refer [validate]]
    [claij.util :refer [def-m2]]
-   [claij.llm.open-router :refer [open-router-async]]
+   [claij.llm.open-router :refer [open-router-async unpack]]
    [claij.fsm :refer [def-fsm make-fsm state-schema xition-schema schema-base-uri uri->schema]]))
 
 ;;------------------------------------------------------------------------------
@@ -52,28 +53,51 @@
         (is (= expected actual))
         (is      (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-B"] "document" "test"})))
         (is (not (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-B"] "document" 0}))))))
-    (testing "state-schema"
-      (let [actual
-            (state-schema fsm state [xition-1 xition-2])
-            expected
-            {"$schema" "https://json-schema.org/draft/2020-12/schema"
-             "$$id" "https://claij.org/schemas/test-fsm/0/test-state-A"
-             "oneOf"
-             [{"properties"
-               {"$schema" {"type" "string"}
-                "$id" {"type" "string"}
-                "id" {"const" ["test-state-A" "test-state-B"]}
-                "document" {"type" "string"}}}
-              {"properties"
-               {"$schema" {"type" "string"}
-                "$id" {"type" "string"}
-                "id" {"const" ["test-state-A" "test-state-C"]}
-                "document" {"type" "number"}}}]}]
-        (is (= expected actual))
-        (is      (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-B"] "document" "test"})))
-        (is (not (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-B"] "document" 0}))))
-        (is      (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-C"] "document" 0})))
-        (is (not (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-C"] "document" "test"}))))))
+    ;; (testing "state-schema: "
+    ;;   (testing "with values"
+    ;;     (let [actual
+    ;;           (state-schema fsm state [xition-1 xition-2])
+    ;;           expected
+    ;;           {"$schema" "https://json-schema.org/draft/2020-12/schema"
+    ;;            "$$id" "https://claij.org/schemas/test-fsm/0/test-state-A"
+    ;;            "oneOf"
+    ;;            [{"properties"
+    ;;              {"$schema" {"type" "string"}
+    ;;               "$id" {"type" "string"}
+    ;;               "id" {"const" ["test-state-A" "test-state-B"]}
+    ;;               "document" {"type" "string"}}}
+    ;;             {"properties"
+    ;;              {"$schema" {"type" "string"}
+    ;;               "$id" {"type" "string"}
+    ;;               "id" {"const" ["test-state-A" "test-state-C"]}
+    ;;               "document" {"type" "number"}}}]}]
+    ;;       (is (= expected actual))
+    ;;       (is      (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-B"] "document" "test"})))
+    ;;       (is (not (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-B"] "document" 0}))))
+    ;;       (is      (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-C"] "document" 0})))
+    ;;       (is (not (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-C"] "document" "test"}))))))
+    ;;   (testing "with refs"
+    ;;     (let [actual
+    ;;           (state-schema fsm state [xition-1 xition-2])
+    ;;           expected
+    ;;           {"$schema" "https://json-schema.org/draft/2020-12/schema"
+    ;;            "$$id" "https://claij.org/schemas/test-fsm/0/test-state-A"
+    ;;            "oneOf"
+    ;;            [{"properties"
+    ;;              {"$schema" {"type" "string"}
+    ;;               "$id" {"type" "string"}
+    ;;               "id" {"const" ["test-state-A" "test-state-B"]}
+    ;;               "document" {"type" "string"}}}
+    ;;             {"properties"
+    ;;              {"$schema" {"type" "string"}
+    ;;               "$id" {"type" "string"}
+    ;;               "id" {"const" ["test-state-A" "test-state-C"]}
+    ;;               "document" {"type" "number"}}}]}]
+    ;;       (is (= expected actual))
+    ;;       (is      (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-B"] "document" "test"})))
+    ;;       (is (not (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-B"] "document" 0}))))
+    ;;       (is      (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-C"] "document" 0})))
+    ;;       (is (not (:valid? (validate {} expected {} {"id" ["test-state-A" "test-state-C"] "document" "test"})))))))
     (testing "$ref to remote schema"
       (let [c2
             {:draft :draft2020-12
@@ -136,7 +160,8 @@
     {"description" "use this to make a request to start/continue a code review"
      "type" "object"
      "properties"
-     {"code" {"$ref" "#/$defs/code"}
+     {"id" {"const" ["mc" "reviewer"]}
+      "code" {"$ref" "#/$defs/code"}
       "notes" {"$ref" "#/$defs/notes"}}
      "additionalProperties" false
      "required" ["code" "notes"]}
@@ -145,7 +170,8 @@
     {"description" "use this to respond with your comments during a code review"
      "type" "object"
      "properties"
-     {"code" {"$ref" "#/$defs/code"}
+     {"id" {"const" ["reviewer" "mc"]}
+      "code" {"$ref" "#/$defs/code"}
       "notes" {"$ref" "#/$defs/notes"}
       "comments" {"$ref" "#/$defs/comments"}}
      "additionalProperties" false
@@ -155,19 +181,23 @@
     {"description" "use this to summarise and exit a code review loop"
      "type" "object"
      "properties"
-     {"code" {"$ref" "#/$defs/code"}
+     {"id" {"const" ["mc" "end"]}
+      "code" {"$ref" "#/$defs/code"}
       "notes" {"$ref" "#/$defs/notes"}}
      "additionalProperties" false
      "required" ["code" "notes"]}}
 
-   "oneOf"
+   "properties"
+   {"content"
+    {"oneOf"
    [{"$ref" "#/$defs/request"}
     {"$ref" "#/$defs/response"}
-    {"$ref" "#/$defs/summary"}]})
+    {"$ref" "#/$defs/summary"}]}}})
 
 (def-fsm
   code-review-fsm
   {"schema" code-review-schema
+   "id" "code-review"
    "prompts" [{"role" "user"
                "content" "You are involved in a code review workflow"}]
    "states"
@@ -200,49 +230,56 @@
      "schema" {"type" "string"}}
     {"id" ["mc" "reviewer"]
      "prompts" []
-     "schema" {"$ref" "https://claij.org/schemas/code-review-schema#/$defs/request"}}
+     "schema" {"properties" {"content" {"$ref" "https://claij.org/schemas/code-review-schema#/$defs/request"}}}}
     {"id" ["reviewer" "mc"]
      "prompts" []
-     "schema" {"$ref" "https://claij.org/schemas/code-review-schema#/$defs/response"}}
+     "schema" {"properties" {"content" {"$ref" "https://claij.org/schemas/code-review-schema#/$defs/response"}}}}
     {"id" ["mc" "end"]
      "prompts" []
-     "schema" {"$ref" "https://claij.org/schemas/code-review-schema#/$defs/summary"}}]})
+     "schema" {"properties" {"content" {"$ref" "https://claij.org/schemas/code-review-schema#/$defs/summary"}}}}]})
 
 (def system-prompts
   [{"role" "system"
-    "content" "You are a cog in a larger machine. All your requests and responses must be received/given in JSON. You will be provided with relevant self-explanatory schemas."}])
+    "content" "You are a cog in a larger machine. All your requests and responses must be received/given in JSON. You will be provided with relevant self-explanatory schemas. Please adhere strictly to your output schema. Do not add any additional properties."}])
 
-(defn llm-action [{fsm-prompts "prompts" :as _fsm}
-                  {ix-schema "schema" ix-prompts "prompts" :as _ix}
-                  {state-prompts "prompts"}
-                  [input & memory]
-                  ox-schema
-                  handler]
-  (let [prompts
-        (concat
-         system-prompts
-         fsm-prompts
-         [{"role" "user"
-           "content" (format "Your input document is conformant to this schema: %s" (write-str ix-schema))}
-          {"role" "user"
-           "content" (format "Your output document must be conformant to this schema: %s" (write-str ox-schema))}]
-         ix-prompts
-         state-prompts
-         [{"role" "user" "content" "The following two json values are arranged in [schema document] pairs. The first is a complete conversational history (newest->oldest). The second is your current input. Please produce your output accordingly"}
-          {"role" "user"  "content" (write-str (vec memory))}
-          {"role" "user"  "content" (write-str input)}])]
+(defn llm-action
+  ([prompts
+    ox-schema
+    handler]
 
-    (open-router-async
+   ;;(clojure.pprint/pprint ox-schema)
+   ;;(clojure.pprint/pprint prompts)
+   
+   (open-router-async
      ;; "anthropic" "claude-sonnet-4.5" ;; markdown
-     ;; "x-ai" "grok-code-fast-1" ;; markdown
-     ;; "openai"    "gpt-5-codex" ;; didn't conform
-     "google" "gemini-2.5-flash" ;; markdown
-     ox-schema
-     prompts
-     (fn [output]
-       (if-let [es (handler output)]
-         (log/error es)
-         nil)))))
+     ;; "x-ai" "grok-code-fast-1" ;; markdown - should work
+    ;; "openai"    "gpt-5-codex" ;; didn't conform - should work
+    "openai"    "gpt-4o" ;; didn't conform - should work
+    ;; "google" "gemini-2.5-flash" ;; should work
+    ox-schema
+    prompts
+    (fn [output]
+      (if-let [es (handler output)]
+        (log/error es)
+        nil))))
+  ([{fsm-prompts "prompts" :as _fsm}
+    {ix-schema "schema" ix-prompts "prompts" :as _ix}
+    {state-prompts "prompts"}
+    [input & memory]
+    ox-schema
+    handler]
+   (let [prompts
+         (concat
+          system-prompts
+          fsm-prompts
+          [{"role" "user"
+            "content" (format "Your output document must be strictly conformant to this schema: %s" (write-str ox-schema))}]
+          ix-prompts
+          state-prompts
+          [{"role" "user" "content" "The following two json values are arranged in [schema document] pairs. The first is a complete conversational history (newest->oldest). The second is your current input. Please produce your output accordingly"}
+           {"role" "user"  "content" (write-str (vec memory))}
+           {"role" "user"  "content" (write-str input)}])]
+     (llm-action prompts ox-schema handler))))
 
 (def code-review-actions
   {"llm" llm-action})
@@ -250,12 +287,10 @@
 (comment
   (make-fsm code-review-actions code-review-fsm "mc")
   (def c (first *1))
-  (clojure.core.async/>!! c [[{"type" "string"} "I have this piece of Clojure code to calculate the fibonacci series - can we improve it through a code review?: `(def fibs (lazy-seq (cons 0 (cons 1 (map + fibs (rest fibs))))))`"]])
-
-  
-  (clojure.core.async/>!! c [[{"type" "string"} {"id" ["" "mc"] "document" "I have this piece of Clojure code to calculate the fibonacci series - can we improve it through a code review?: `(def fibs (lazy-seq (cons 0 (cons 1 (map + fibs (rest fibs))))))`"}]]))
-
-;;{"$schema" "http://megalodon:8080/schemas/FSM-ID/FSM-VERSION/start/transitions" "$id" "TODO" "id" ["start" "meeting"] "document" "how are you ?"}
+  (clojure.core.async/>!!
+   c
+   [[{"properties" {"id" {"type" "array" "item" {"type" "string"}} "document" {"type" "string"}}}
+     {"id" ["" "mc"] "document" "I have this piece of Clojure code to calculate the fibonacci series - can we improve it through a code review?: `(def fibs (lazy-seq (cons 0 (cons 1 (map + fibs (rest fibs))))))`"}]]))
 
 ;; TODO:
 ;; - or store schema separately and add to context so we can ref it from the xititions
@@ -265,4 +300,83 @@
 
 ;;------------------------------------------------------------------------------
 
+;; (clj-http.client/post
+;;  (str api-url "/chat/completions")
+;;  {:async? true
+;;   :headers headers
+;;   :body
+;;   (clojure.data.json/write-str
+;;    {:model (str provider "/" model)
+;;     :response_format
+;;     {:type "json_schema"
+;;      :json_schema
+;;      {:name "weather"
+;;       :strict true
+;;       :schema {"type" "object",
+;;                "properties"
+;;                {"location"
+;;                 {"type" "string", "description" "City or location name"},
+;;                 "temperature"
+;;                 {"type" "number", "description" "Temperature in Celsius"},
+;;                 "conditions"
+;;                 {"type" "string", "description" "Weather conditions description"}},
+;;                "required" ["location" "temperature" "conditions"],
+;;                "additionalProperties" false}}}
+;;     :messages {"role" "user", "content" "What's the weather like in London?"}})}
+;;  (fn [{b :body}] (let [{[{{c "content"} "message"}] "choices"} (clojure.data.json/read-str b)] (handler (clojure.data.json/read-str (strip-md-json c)))))
+;;  (fn [exception] (println "Error:" (.getMessage exception))))
 
+;; (defn unpack [{b :body}]
+;;   (let [{[{{c "content"} "message"}] "choices"} (clojure.data.json/read-str (clojure.string/trim b))]
+;;     c
+;;     ;;(clojure.data.json/read-str (strip-md-json c))
+;;     ))
+
+(defn trace [m v]
+  (prn m v)
+  v)
+
+(deftest structured-data-integration-test
+  (let [schema
+        {"$id" "https://claij.org/schemas/structured-data-integration-test"
+         "type" "object",
+         "properties"
+         {"location"
+          {"type" "string", "description" "City or location name"},
+          "temperature"
+          {"type" "number", "description" "Temperature in Celsius"},
+          "conditions"
+          {"type" "string", "description" "Weather conditions description"}},
+         "required" ["location" "temperature" "conditions"],
+         "additionalProperties" false}
+        prompts
+        [{"role" "user", "content" "What's the weather like in London?"}]]
+    (doseq [[provider model] [["openai" "gpt-4o"]
+                              ;; ["x-ai" "grok-code-fast-1"]
+                              ;; ["x-ai" "grok-4"]
+                              ;; ["google" "gemini-2.5-flash"]
+                              ;; failing
+                              ;;["openai" "gpt-5-pro"]
+                              ;;["anthropic" "claude-sonnet-4.5"] ;; https://forum.bubble.io/t/anthropic-json-mode-tools-via-the-api-connector-tutorial/331283
+                              ]]
+      (testing "weather"
+        (testing (str provider "/" model)
+          (is (:valid? (validate {:draft :draft7} schema {} (let [p (promise)] (open-router-async provider model schema prompts (partial deliver p)) @p)))))))))
+
+
+(deftest code-review-schema-test
+  (testing "code-review"
+    (let [[provider model] ["openai" "gpt-4o"]
+          schema code-review-schema
+          prompts
+          [{"role" "user"
+            "content" "I' going to give you a pair of JSON schema (to help you understand the document) and conformant document (to be understood as my request to you)."}
+           {"role" "user"
+            "content" (write-str
+                       [{"properties" {"id" {"type" "array" "item" {"type" "string"}} "document" {"type" "string"}}}
+                        {"id" ["" "mc"] "document" "I have this piece of Clojure code to calculate the fibonacci series - I'd like you to request a code review to improve it?: `(def fibs (lazy-seq (cons 0 (cons 1 (map + fibs (rest fibs))))))`"}])}]]
+      (testing (str provider "/" model)
+        (let [p (promise)]
+          (open-router-async provider model schema prompts (partial deliver p))
+          (println "HERE:" @p)
+          (is (:valid? (validate {:draft :draft7} schema {} @p))))))))
