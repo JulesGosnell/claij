3186822e56f0f27f4116220a6580475b57711ad3
jules <jules_gosnell@yahoo.com>
2025-11-04 15:26:12 +0000

    integrate prompts with llm call

--- test/claij/fsm_test.clj
+++ test/claij/fsm_test.clj
@@ -1,6 +1,7 @@
 (ns claij.fsm-test
   (:require
    [clojure.tools.logging :as log]
+   [clojure.string :refer [join]]
    [clojure.data.json :refer [write-str]]
    [clojure.pprint :refer [pprint]]
    [clojure.test :refer [deftest testing is]]
@@ -196,29 +197,21 @@
   code-review-fsm
   {"schema" code-review-schema
    "id" "code-review"
-   "prompts" [{"role" "user"
-               "content" "You are involved in a code review workflow"}]
+   "prompts" ["You are involved in a code review workflow"]
    "states"
    [{"id" "mc"
      "action" "llm"
      "prompts"
-     [{"role" "user"
-       "content" "You are an MC orchestrating a code review."}
-      {"role" "user"
-       "content" "You should always request at least one review, then merge any useful code changes and/or refactor to take any useful comments into consideration and ask for further review."}
-      {"role" "user"
-       "content" "Keep requesting and reacting to reviews until you are satisfied that you are no longer turning up useful issues. Then please summarise your findings with the final version of the code."}]}
+     ["You are an MC orchestrating a code review."
+      "You should always request at least one review, then merge any useful code changes and/or refactor to take any useful comments into consideration and ask for further review."
+      "Keep requesting and reacting to reviews until you are satisfied that you are no longer turning up useful issues. Then please summarise your findings with the final version of the code."]}
     {"id" "reviewer"
      "action" "llm"
      "prompts"
-     [{"role" "user"
-       "content" "You are a code reviewer"}
-      {"role" "user"
-       "content" "You will be requested to review some code. Please give the following careful consideration - clarity, simplicity, beauty, efficiency, intuitiveness, laconicity, idiomaticity, correctness, security, and maintainability - along with measures of your own choice."}
-      {"role" "user"
-       "content" "You can change the code, add your comments to the review, along with general notes in your response."}
-      {"role" "user"
-       "content" "Please do not feel that you have to find fault. If you like the code, just respond thus so that the MC can terminate the review."}]}
+     ["You are a code reviewer"
+      "You will be requested to review some code. Please give the following careful consideration - clarity, simplicity, beauty, efficiency, intuitiveness, laconicity, idiomaticity, correctness, security, and maintainability - along with measures of your own choice."
+      "You can change the code, add your comments to the review, along with general notes in your response."
+      "Please do not feel that you have to find fault. If you like the code, just respond thus so that the MC can terminate the review."]}
 
     {"id" "end"
      "action" "end"}]
@@ -251,21 +244,21 @@
 (defn make-prompts [{fsm-schema "schema" fsm-prompts "prompts" :as _fsm} {ix-schema "schema" ix-prompts "prompts" :as _ix} {state-prompts "prompts"} memory ox-schema ix-document]
   (concat
    [{"role" "system"
-     "content" (str
-                "All your requests and responses will be JSON with accompanying explanatory JSON schemas."
-                "You are being given the following reference JSON schema. Later schemas may refer to $defs in this one:" (write-str fsm-schema) "."
-                "Requests will arrive as [schema, document] pairs - The schema describes the document.You must respond to the contents of the document")}]
-   fsm-prompts
-   ix-prompts
-   state-prompts
+     "content" (join
+                "\n"
+                (concat
+                 ["All your requests and responses will be in JSON."
+                  "You are being given the following reference JSON schema. Later schemas may refer to $defs in this one:" (write-str fsm-schema) "."
+                  "Requests will arrive as [INPUT-SCHEMA, DOCUMENT, OUTPUT-SCHEMA] triples."
+                  "The INPUT-SCHEMA describes the structure of the DOCUMENT."
+                  "You must respond to the contents of the DOCUMENT."
+                  "Your response must be a single JSON document that is STRICTLY CONFORMANT (please pay particular attention to the \"id\" which must be present as a pair of strings) to the OUTPUT-SCHEMA:"]
+                 fsm-prompts
+                 ix-prompts
+                 state-prompts))}]
    memory
    [{"role" "user"
-     "content"
-     (str
-      "Your response must be a single JSON document that is STRICTLY CONFORMANT (please pay particular attention to the \"id\" which must be present as a pair of strings) to this schema:"
-      (write-str ox-schema))}
-    {"role" "user"
-     "content" (write-str [ix-schema ix-document])}]))
+     "content" (write-str [ix-schema ix-document ox-schema])}]))
 
 (defn llm-action
   ([prompts ox-schema handler]
@@ -403,11 +396,11 @@
 (deftest code-review-schema-test
   (testing "code-review"
     (doseq [[provider model]
-            [;; ["openai" "gpt-5-pro"]
-             ;; ["google" "gemini-2.5-flash"]
-             ;; ["x-ai" "grok-4"]
+            [["openai" "gpt-5-codex"]
+             ["google" "gemini-2.5-flash"]
+             ["x-ai" "grok-code-fast-1"]
              ["anthropic" "claude-sonnet-4.5"]
-             ]]
+             ["meta-llama" "llama-4-maverick:free"]]]
       (testing (str provider "/" model)
         (let [schema code-review-schema
               prompts (make-prompts
@@ -418,9 +411,8 @@
                        {"oneOf" [{"$ref" "#/$defs/request"}
                                  {"$ref" "#/$defs/summary"}]}
                        {"id" ["" "mc"] "document" "I have this piece of Clojure code to calculate the fibonacci series - can we improve it through a code review?: `(def fibs (lazy-seq (cons 0 (cons 1 (map + fibs (rest fibs))))))`"})]
-          (testing (str provider "/" model)
-            (let [p (promise)]
-              (log/info @(open-router-async provider model schema prompts (partial deliver p) (partial deliver p)))
-              (is (:valid? (validate {:draft :draft7} schema {} @p))))))))))
+          (let [p (promise)]
+            (log/info (deref (open-router-async provider model schema prompts (partial deliver p) (partial deliver p)) 60000 "timed out after 60s"))
+            (is (:valid? (validate {:draft :draft7} schema {} (deref p 60000 "timed out after 60s"))))))))))
 
 ;; the oneOf should not be in the fsm level schema but added by state-schema
